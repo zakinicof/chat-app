54caa97f512e818cefeed1da21b5b2aeb4b693fb
zakinicof <72780750+zakinicof@users.noreply.github.com>
2020-10-16 15:37:06 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,19 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  # ストロングパラメーター
+  private
+
+  def room_params
+    # 配列[]に対しての保存を許可
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
