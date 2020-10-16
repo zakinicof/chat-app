d6b2d2dd8045af3cdfc83e23ecb485d51d3ee20b
zakinicof <72780750+zakinicof@users.noreply.github.com>
2020-10-16 12:37:31 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # nameが空の状態でユーザー登録できないように設定
+  validates :name, presence: true
+
 end
