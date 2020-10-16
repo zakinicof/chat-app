8bbc27d71d417d3a0cd81baaa61cce44a07e3963
zakinicof <72780750+zakinicof@users.noreply.github.com>
2020-10-16 17:44:17 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,22 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+  # ストロングパラメーター
+  private
+
+  def message_params
+    # 配列[]に対しての保存を許可
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  # どのルームで投稿されたメッセージなのかをパスから判断できるようにしたいので、ルーティングのネストを利用
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
